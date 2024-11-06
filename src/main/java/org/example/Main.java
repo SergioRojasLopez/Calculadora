b681b9925b99ae40a2d2441fae336591f297ae33
SergioRojasLopez <115539369+SergioRojasLopez@users.noreply.github.com>
2024-11-06 08:58:10 +0100

    método sumar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
 public class Main {
+
+    private static int sumar(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
