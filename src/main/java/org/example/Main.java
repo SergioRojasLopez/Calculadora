bbecf7fcec41b138adc06a0fbf284dac18b8d6f6
elpapass <115544415+elpapass@users.noreply.github.com>
2024-11-06 08:58:11 +0100

    Método restar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
 public class Main {
+
+    public static int restar(int valor1, int valor2){
+        return valor1 - valor2;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
